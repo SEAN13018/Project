9b6c351e722826825985acdbb4b0fd65b9689c80
Sean N <13018@burnside.school.nz>
2016-06-02 14:39:09 +1200

    2:39

--- programV0.1.py
+++ programV0.1.py
@@ -1,3 +1,18 @@
 # Menu V0.1 
 # 02/06/16
 # Sean Nichols
+
+from tkinter import *
+GUI = Tk()
+GUI.geometry("680x500")
+GUI.title("Menu Program")
+topFrame = Frame(GUI)
+topFrame.pack()
+bottomFrame = Frame(GUI)
+bottomFrame.pack(side=BOTTOM)
+GUI.configure(background='light blue')
+
+menu = Menu(GUI)
+GUI.config(menu=menu)
+subMenu = Menu(menu)
+
